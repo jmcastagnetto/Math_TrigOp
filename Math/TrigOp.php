df098463e33f8b0e7840b99f339863905f6da4ff
Jesus M. Castagnetto <jesus@upch.pe>
2013-10-28 10:40:33 -0500

    changing the license to BSD-3

--- Math/TrigOp.php
+++ Math/TrigOp.php
@@ -1,23 +1,12 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 5                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/3_0.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Jesus M. Castagnetto <jmcastagnetto@php.net>                |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
+/**
+ * PHP version 5
+ * License: BSD 3-Clause
+ *
+ * @author	Jesus M. Castagnetto <jmcastagnetto@php.net>
+ * @copyright 1997-2013 Jesus M. Castagnetto
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ */
 
 namespace PEAR2\Math;
 
